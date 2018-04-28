d82898fdc5d4765af582657b3fe8e9b81e12be90
Mavroidis Mavroidis <mnm48@zips.uakron.edu>
2018-04-28 13:55:37 -0400

    reformating

--- sc435.cpp
+++ sc435.cpp
@@ -1,8 +1,12 @@
 /*
-Seam Carving Project
 Mavroidis Mavroidis 
+4/27/2018
+Final Project
+Algorithms
+Seam Carving 
 */
-#include <iostream>
+
+
 #include <fstream>
 #include <iosfwd>
 #include <map>
@@ -12,64 +16,68 @@ Mavroidis Mavroidis
 #include <cstdlib>
 #include <iostream>
 
-//Seam Carving Class
+
+// Seam carving Class
 class SC {
 public:
 
+	//Constructor and functions
 	SC() = default;
-
-	void print(std::vector<std::vector<int>> mtx);
 	void get_hdr(std::ifstream& img);
 	void pop_pix_mtx(std::ifstream& img);
 	void pop_enr_mtx();
 	void pop_cume_mtx();
 	void find_v_seams();
-	auto find_min_col();
+	void pop_cume_h();
+	void rotate(int a);
 	void find_h_seams();
 	void remove_v_seams(int v_seams);
-	void remove_h_seams(int horizontal_seams);
-	void write(std::ofstream& output);
-	void rotate();
+	void remove_h_seams(int h_seams);
+	void write(std::ofstream& out);
 
 private:
-
 	std::vector<std::vector<int>> pix_mtx;
 	std::vector<std::vector<int>> enr_mtx;
 	std::vector<std::vector<int>> cume_mtx;
-	std::vector<std::vector<int>> rot_mtx;
+	std::vector<std::vector<int>> pix_rot;
+	std::vector<std::vector<int>> cume_rot;
 	std::map<int, int> pos;
-	std::string hdr, dim, grey_max;
+	std::string hdr, dim, max_gray;
 	int	w, h;
 };
 
 
+//Main Function Loop
 int main(int argc, char** argv) {
 	std::ifstream img;
 	std::ofstream img_proc;
+	int v_seams, h_seams;
 
 	try {
-		//Check for correct number of arguments
+
 		if (argc != 4)
-			throw "Please use the following format. file_name number_of_vertical_seams number_of_horizontal_seams";
+			throw "Please enter the following to run program, file_Name num_vert_seams num_hor_seams";
 		else {
-			//Check that number of seams to be removed is positive
-			if (atoi(argv[2]) <= 0 || atoi(argv[3]) <= 0)
-				throw "Number of vertical and horizontal seams must be greater than zero";
-			//File Name
+
+			if (atoi(argv[2]) < 0 || atoi(argv[3]) < 0)
+				throw "Enter Positive number of seams to remove";
+			else {
+				v_seams = atoi(argv[2]);
+				h_seams = atoi(argv[3]);
+			}
+
 			std::string f_name = argv[1];
 			img.open(f_name.c_str(), std::ios::binary);
-
-			//change file name
 			f_name.erase(f_name.length() - 4, 4);
 			f_name = f_name + "_processed.pgm";
 			img_proc.open(f_name.c_str(), std::ios::binary);
 
-			//Error handling for opening and creating files
+			// Check to ensure proper io
 			if (!img.is_open()) {
-				throw "Failed to open file";
+				throw "Could not open img file";
 			}
 			if (!img_proc.is_open()) {
-				throw "Failed to create output file";
+				throw "Could not create out file";
 			}
 		}
 	}
@@ -77,37 +85,29 @@ int main(int argc, char** argv) {
 		std::cout << "Error: " << err << std::endl;
 	}
 
-	//Create Seam carver object and do the manipulations to the file and output 
-	SC sc;
-	sc.get_hdr(img);
-	sc.pop_pix_mtx(img);
-	sc.rotate();
-	sc.remove_v_seams(atoi(argv[2]));
-	sc.remove_h_seams(atoi(argv[3]));
-	sc.write(img_proc);
+	//Create the SC object and grab the header attributes
+	SC sc;								
+	sc.get_hdr(img);			
 
+	//Populate the matricies and remove the seams
+	sc.pop_pix_mtx(img);			
+	sc.remove_v_seams(v_seams);			
+	sc.remove_h_seams(h_seams);		
+
+	//Output to file
+	sc.write(img_proc);				
 	img.close();
 	img_proc.close();
-
-
 
 	return 0;
 }
 
 
 
-//Print
-void SC::print(std::vector<std::vector<int>> mtx) {
-	for (int y = 0; y < h; y++) {
-		for (int x = 0; x < w; x++)
-			std::cout << mtx[y][x] << " ";
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-}
-//Gets header from pgm file
+// Get header Info
 void SC::get_hdr(std::ifstream& img) {
 	std::string x, y;
+
 
 	std::getline(img, hdr);
 	while (hdr[0] == '#') {
@@ -115,28 +115,25 @@ void SC::get_hdr(std::ifstream& img) {
 	}
 
 	std::getline(img, dim);
-	
 	while (dim[0] == '#') {
 		std::getline(img, dim);
 	}
-	
-	std::getline(img, grey_max);
-	
-	while (grey_max[0] == '#') {
-		std::getline(img, grey_max);
+
+	std::getline(img, max_gray);
+	while (max_gray[0] == '#') {
+		std::getline(img, max_gray);
 	}
-	
+
 	const char* chars = dim.c_str();
 
 	while (*chars != ' ') {
 		x += *chars++;
 	}
-	
-	for (auto i = dim.size() - x.size() - 1; i < dim.size(); i++) {
+
+	for (int i = dim.size() - x.size() - 1; i < dim.size(); i++) {
 		y += *chars++;
 	}
 
-	//Get width and height
 	w = stoi(x);
 	h = stoi(y);
 }
@@ -145,28 +142,26 @@ void SC::get_hdr(std::ifstream& img) {
 void SC::pop_pix_mtx(std::ifstream& img) {
 
 	pix_mtx.resize(h);
-	
-	//Resize to 2d matrix
 	for (int i = 0; i < h; i++) {
 		pix_mtx[i].resize(w);
 	}
-	
-	//Populate matrix
-	for (int y = 0; y < h; y++)
-		for (int x = 0; x < w; x++)
+
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
 			img >> pix_mtx[y][x];
+		}
+	}
 }
 
-//Populate energy matrix
+// Populate energy matrix
 void SC::pop_enr_mtx() {
 
 	enr_mtx.resize(h);
-	//Resize to 2d matrix
+
 	for (int i = 0; i < h; i++) {
 		enr_mtx[i].resize(w);
 	}
-	
-	//Handles All the different cases for where you are in matrix
+
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
 
@@ -217,210 +212,235 @@ void SC::pop_enr_mtx() {
 
 
 			else {
-				enr_mtx[y][x] = abs(pix_mtx[y][x] - pix_mtx[y][x - 1]) + abs(pix_mtx[y][x] - pix_mtx[y][x + 1]) + abs(pix_mtx[y][x] - pix_mtx[y - 1][x]) + abs(pix_mtx[y][x] - pix_mtx[y + 1][x]);
+				enr_mtx[y][x] = abs(pix_mtx[y][x] - pix_mtx[y][x - 1]) + abs(pix_mtx[y][x] - pix_mtx[y][x + 1])	+ abs(pix_mtx[y][x] - pix_mtx[y - 1][x]) + abs(pix_mtx[y][x] - pix_mtx[y + 1][x]);
 			}
 		}
 	}
 }
 
-//Populate cumulative energy matrix
+// Populate cumulative mtx
 void SC::pop_cume_mtx() {
-	cume_mtx.resize(h);
-	for (int i = 0; i < h; i++)
-		cume_mtx[i].resize(w);
 
+	cume_mtx.clear();
+	cume_mtx.resize(h);
+	
+	for (int i = 0; i < h; i++) {
+		cume_mtx[i].resize(w);
+	}
 
 	for (int x = 0; x < w; x++) {
 		cume_mtx[0][x] = enr_mtx[0][x];
 	}
-
 	for (int y = 1; y < h; y++) {
 		for (int x = 0; x < w; x++) {
 
-			
+		
 			if (x == 0) {
-				cume_mtx[y][x] = enr_mtx[y][x] + std::min(enr_mtx[y - 1][x], enr_mtx[y - 1][x + 1]);
-			}
-			
-			else if (x == w - 1) {
-				cume_mtx[y][x] = enr_mtx[y][x] + std::min(enr_mtx[y - 1][x], enr_mtx[y - 1][x - 1]);
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(cume_mtx[y - 1][x], cume_mtx[y - 1][x + 1]);
 			}
 
+		
+			else if (x == w - 1) {
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(cume_mtx[y - 1][x], cume_mtx[y - 1][x - 1]);
+			}
+
+		
 			else {
-				cume_mtx[y][x] = enr_mtx[y][x] + std::min(std::min(enr_mtx[y - 1][x - 1], enr_mtx[y - 1][x]), enr_mtx[y - 1][x + 1]);
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(std::min(cume_mtx[y - 1][x - 1], cume_mtx[y - 1][x]), cume_mtx[y - 1][x + 1]);
 			}
 		}
 	}
-	print(cume_mtx);
 }
-//Find the vertical seams 
+
+// Find Vertical seams
 void SC::find_v_seams() {
+
 	std::vector<int>::iterator it;
+	pos.clear();
 
 	it = std::min_element(cume_mtx[h - 1].begin(), cume_mtx[h - 1].end());
 	pos[h - 1] = std::distance(std::begin(cume_mtx[h - 1]), it);
 
 	for (int row = h - 1; row > 0; row--) {
-		
+
 		if (pos[row] == 0) {
 			it = std::min_element(cume_mtx[row - 1].begin(), cume_mtx[row - 1].begin() + 2);
 			pos[row - 1] = std::distance(std::begin(cume_mtx[row - 1]), it);
 		}
-
 		
 		else if (pos[row] == w - 1) {
-			it = std::min_element(cume_mtx[row - 1].begin() + pos[row] - 1, cume_mtx[row - 1].end());
+			it = std::min_element(cume_mtx[row - 1].end() - 2, cume_mtx[row - 1].end());
 			pos[row - 1] = std::distance(std::begin(cume_mtx[row - 1]), it);
 		}
 
-		
 		else if (w <= 3) {
 			it = std::min_element(cume_mtx[row - 1].begin(), cume_mtx[row - 1].end());
 			pos[row - 1] = std::distance(std::begin(cume_mtx[row - 1]), it);
 		}
 
-		
 		else {
-			it = std::min_element(cume_mtx[row - 1].begin() + pos[row] - 1, cume_mtx[row - 1].begin() + pos[row] + 1);
+			it = std::min_element(cume_mtx[row - 1].begin() + pos[row] - 1, cume_mtx[row - 1].begin() + pos[row] + 2);
 			pos[row - 1] = std::distance(std::begin(cume_mtx[row - 1]), it);
 		}
 	}
 }
 
-//Removing vertical seams
+// Remove vertical seams
 void SC::remove_v_seams(int v_seams) {
-	while (v_seams > 0) {
 
-		pop_enr_mtx();
-		pop_cume_mtx();
-		find_v_seams();
+	while (v_seams > 0) {
+		pop_enr_mtx();		
+		pop_cume_mtx(); 
+		find_v_seams();			
 
 		for (auto i : pos) {
 			pix_mtx[i.first].erase(pix_mtx[i.first].begin() + i.second);
-			cume_mtx[i.first].erase(cume_mtx[i.first].begin() + i.second);
 		}
 
 		w--;
 		v_seams--;
 	}
-
-	print(pix_mtx);
 }
 
-//Find the min columns
-auto SC::find_min_col() {
+// Populate cumulative matrix 
+void SC::pop_cume_h() {
 
-	std::vector<int> val;
-	int column = w - 1;
-
-	for (int row = 0; row < h; row++) {
-		val.push_back(cume_mtx[row][column]);
+	cume_mtx.clear();
+	cume_mtx.resize(h);
+	for (int i = 0; i < h; i++) {
+		cume_mtx[i].resize(w);
+	}
+	
+	for (int y = 0; y < h; y++) {
+		cume_mtx[y][0] = enr_mtx[y][0];
 	}
 
-	auto min_elem = std::min_element(val.begin(), val.end());
+	for (int x = 1; x < w; x++) {
+		for (int y = 0; y < h; y++) {
 
-	for (int row = 0; row < h; row++) {
-		if (cume_mtx[row][column] == *min_elem) {
-			return cume_mtx[row].begin() + column;
+			if (y == 0) {
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(cume_mtx[y][x - 1], cume_mtx[y + 1][x - 1]);
+			}
+
+			else if (y == h - 1) {
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(cume_mtx[y][x - 1], cume_mtx[y - 1][x - 1]);
+			}
+
+			else {
+				cume_mtx[y][x] = enr_mtx[y][x] + std::min(std::min(cume_mtx[y + 1][x - 1], cume_mtx[y][x - 1]), cume_mtx[y - 1][x - 1]);
+			}
 		}
 	}
 }
 
-void SC::rotate() {
-	rot_mtx.resize(w);
-//	rotated_matrix.resize(width);
-	for (int i = 0; i < w; i++)
-		rot_mtx[i].resize(h);
+// Rotate matrix 
+void SC::rotate(int a) {
 
-	for (int i = 0; i < w; i++)
-		for (int j = 0; j < h; j++)
-			rot_mtx[i][j] = cume_mtx[h - 1 - j][i];
+	// 90 c
+	if (a == 0) {
 
+		cume_rot.resize(w);
+		pix_rot.resize(w);
 
+		for (int i = 0; i < w; i++) {
+			cume_rot[i].resize(h);
+			pix_rot[i].resize(h);
+		}
 
-	//Print 
-	for (int y = 0; y <w; y++) {
-		for (int x = 0; x < h; x++)
-			std::cout << rot_mtx[y][x] << " ";
-		std::cout << std::endl;
+		for (int i = 0; i < w; i++)
+			for (int j = 0; j < h; j++) {
+				cume_rot[i][j] = cume_mtx[h - 1 - j][i];
+				pix_rot[i][j] = pix_mtx[h - 1 - j][i];
+			}
 	}
-	std::cout << std::endl;
+
+	// 90 cc
+	else if (a == 1) {
+
+		pix_mtx.clear();
+		pix_mtx.resize(h);
+
+		for (int i = 0; i < h; i++) {
+			pix_mtx[i].resize(w);
+		}
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				pix_mtx[i][j] = pix_rot[j][h - 1 - i];
+			}
+		}
+	}
 
 }
 
-//Find horizontal seams 
+// Find h seams
 void SC::find_h_seams() {
 
-	std::vector<std::vector<int>>::iterator it;
-	int row;
-	std::vector<int> comp;
-	auto min_iter = find_min_col();
+	std::vector<int>::iterator it;
 	pos.clear();
 
+	it = std::min_element(cume_rot[w - 1].begin(), cume_rot[w - 1].end());
+	pos[w - 1] = std::distance(std::begin(cume_rot[w - 1]), it);
 
+	for (int row = w - 1; row > 0; row--) {
 
-
-	for (int y = 0; y < h; y++) {
-		if (cume_mtx[y].begin() <= min_iter && min_iter < cume_mtx[y].end()) {
-			row = y;
-		}
-	}
-
-	for (int column = w - 1; column > 0; column--) {
-
-		
-		if (row == 0) {
-
+		if (pos[row] == 0) {
+			it = std::min_element(cume_rot[row - 1].begin(), cume_rot[row - 1].begin() + 2);
+			pos[row - 1] = std::distance(std::begin(cume_rot[row - 1]), it);
 		}
 
-		
-		else if (row == h - 1) {
-
+		else if (pos[row] == h - 1) {
+			it = std::min_element(cume_rot[row - 1].end() - 2, cume_rot[row - 1].end());
+			pos[row - 1] = std::distance(std::begin(cume_rot[row - 1]), it);
 		}
 
-		
 		else if (h <= 3) {
-
+			it = std::min_element(cume_rot[row - 1].begin(), cume_rot[row - 1].end());
+			pos[row - 1] = std::distance(std::begin(cume_rot[row - 1]), it);
 		}
 
-		// Otherwise
 		else {
-
+			it = std::min_element(cume_rot[row - 1].begin() + pos[row] - 1, cume_rot[row - 1].begin() + pos[row] + 2);
+			pos[row - 1] = std::distance(std::begin(cume_rot[row - 1]), it);
 		}
 	}
 }
 
-void SC::remove_h_seams(int horizontal_seams) {
-	while (horizontal_seams > 0) {
+// Remove h seams
+void SC::remove_h_seams(int h_seams) {
+
+	while (h_seams > 0) {
 		pop_enr_mtx();
-		pop_cume_mtx();
+		pop_cume_h();
+		rotate(0);
 		find_h_seams();
 
 		for (auto i : pos) {
-			pix_mtx[i.first].erase(pix_mtx[i.first].begin() + i.second);
-			cume_mtx[i.first].erase(cume_mtx[i.first].begin() + i.second);
+			pix_rot[i.first].erase(pix_rot[i.first].begin() + i.second);
 		}
 
+		h_seams--;
 		h--;
-		horizontal_seams--;
+		rotate(1);
 	}
-
-	print(pix_mtx);
 }
 
-void SC::write(std::ofstream& output) {
-	std::vector<std::vector<int>>::iterator row;
-	std::vector<int>::iterator column;
+//Output to file 
+void SC::write(std::ofstream& out) {
 
-	output << "P2\n";
-	output << w << " " << h << "\n";
-	output << grey_max;
+	std::vector<std::vector<int>>::iterator row;
+	std::vector<int>::iterator col;
+
+	out << "P2\n";
+	out << "#\n";
+	out << w << " " << h << "\n";
+	out << max_gray << "\n";
 
 	for (row = pix_mtx.begin(); row != pix_mtx.end(); row++) {
-		for (column = row->begin(); column != row->end(); column++) {
-			output << *column << " ";
+		for (col = row->begin(); col != row->end(); col++) {
+			out << *col << " ";
 		}
-		output << "\n";
+		out << "\n";
 	}
-	output << "\n";
+	out << "\n";
 }
